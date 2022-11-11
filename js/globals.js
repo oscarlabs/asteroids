07397e317f3bddbe4dc167a9913cc95ea3dcd479
oscarlabs <opinedals@icloud.com>
2022-11-10 20:27:55 -0500

    fix audio

--- js/globals.js
+++ js/globals.js
@@ -19,8 +19,14 @@ let totalPoints = 0;
 let asteroidsDestroyed = 0;
 let asteroidsDodged = 0;
 
-const myLaserAudio = document.createElement('audio');
-myLaserAudio.src = '../images/laser.mp3';
+//const myLaserAudio = document.createElement('audio');
+//myLaserAudio.src = '../images/laser.mp3';
 
-const myBackgroundAudio = document.createElement('audio');
-myBackgroundAudio.src = '../images/heroic.mp3';
+//const myBackgroundAudio = document.createElement('audio');
+//myBackgroundAudio.src = '../images/heroic.mp3';
+
+const myBackgroundAudio = document.getElementById('backgroundAudio');
+myBackgroundAudio.style.display = 'none';
+
+const myLaserAudio = document.getElementById('laserAudio');
+myLaserAudio.style.display = 'none';
